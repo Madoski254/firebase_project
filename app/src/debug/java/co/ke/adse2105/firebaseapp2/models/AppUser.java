8ab5ecacb8c0a2f08752ab8d297b8369bcfb713d
Gabriel <81635009+Madoski254@users.noreply.github.com>
2022-11-18 11:06:17 +0300

    All unversioned files

---
+++ app/src/debug/java/co/ke/adse2105/firebaseapp2/models/AppUser.java
@@ -0,0 +1,64 @@
+package co.ke.adse2105.firebaseapp2.models;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AppUser implements Serializable
+{
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Date joinDate;
+
+
+    public AppUser(String firstname, String lastname, String email, String password ) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.joinDate = Calendar.getInstance().getTime();
+    }
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+}
