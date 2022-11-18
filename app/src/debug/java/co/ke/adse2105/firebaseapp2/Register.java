fc71bde925b8fb00667d45346cfefd7d7bbc3bf9
Gabriel <81635009+Madoski254@users.noreply.github.com>
2022-11-18 11:21:36 +0300

    changed on Register.java.java

--- app/src/debug/java/co/ke/adse2105/firebaseapp2/Register.java
+++ app/src/debug/java/co/ke/adse2105/firebaseapp2/Register.java
@@ -112,7 +112,6 @@ public class Register extends AppCompatActivity {
             return false;
         }
 
-
         if (email.isEmpty() || !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+" +
                 "(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f" +
                 "\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
